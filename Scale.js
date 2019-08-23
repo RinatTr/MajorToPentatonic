f3dd29852391b34fb5b9800affd308c24f9b61ea
RinatTr <rinatreg@gmail.com>
2019-08-23 14:47:25 -0400

    addSharps function

--- Scale.js
+++ Scale.js
@@ -1,9 +1,31 @@
 const { sharps, flats, notes } = require('./data.js');
 
 class Scale {
-  constructor() {
+  constructor(root_input) {
     this.sharps = sharps;
     this.flats = flats;
-    this.notes = notes;
+    this.notes_display = notes;
+    this.root = root_input;
   }
+
+  addSharps() {
+    let root = this.root[0].toLowerCase();
+    let sharpIdx = 3;
+    for (let i = 0; i < this.sharps[root]; i++) {
+      sharpIdx = sharpIdx % this.notes_display.length
+      this.notes_display[sharpIdx] = this.notes_display[sharpIdx] + "#";
+      sharpIdx += 4;
+    }
+    console.log(this.notes_display)
+  }
+
+  addFlats() {
+
+  }
+
+  setRootPosition() {
+
+  }
+
 }
+let scale = new Scale("a").addSharps()
