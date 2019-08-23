ee8e665b50a0321fe9d6d6578ff8a6f2cc24f68e
RinatTr <rinatreg@gmail.com>
2019-08-23 15:11:17 -0400

    addAccidentals function

--- Scale.js
+++ Scale.js
@@ -20,7 +20,39 @@ class Scale {
   }
 
   addFlats() {
+    let root = this.root[0].toLowerCase();
+    let flatIdx = 6;
+    for (let i = 0; i < this.flats[root]; i++) {
+      flatIdx = flatIdx % this.notes_display.length
+      this.notes_display[flatIdx] = this.notes_display[flatIdx] + "b";
+      flatIdx += 3;
+    }
+    console.log(this.notes_display)
 
+  }
+
+  addAccidentals(type) {
+    let root = this.root[0].toLowerCase();
+    let accidentalIdx, reference, symbol, interval;
+    switch(type) {
+      case "#":
+        accidentalIdx = 3;
+        reference = this.sharps;
+        symbol = "#";
+        interval = 4;
+        break;
+      case "b":
+        accidentalIdx = 6;
+        reference = this.flats;
+        symbol = "b";
+        interval = 3;
+        break;
+    }
+    for (let i = 0; i < reference[root]; i++) {
+      accidentalIdx = accidentalIdx % this.notes_display.length;
+      this.notes_display[accidentalIdx] = this.notes_display[accidentalIdx] + symbol;
+      accidentalIdx += interval;
+    }
   }
 
   setRootPosition() {
@@ -28,4 +60,4 @@ class Scale {
   }
 
 }
-let scale = new Scale("a").addSharps()
+let scale = new Scale("g").addAccidentals("#")
