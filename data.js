071c4b801b4e09580c50ea4e03864378fd5b479c
RinatTr <rinatreg@gmail.com>
2019-08-23 13:11:25 -0400

    init data

---
+++ data.js
@@ -0,0 +1,31 @@
+const sharps = {
+  c: 0,
+  g: 1,
+  d: 2,
+  a: 3,
+  e: 4,
+  b: 5,
+  fs: 6,
+  cs: 7,
+  gs: 8,
+  ds: 9,
+  as: 10
+};
+
+const flats = {
+  c: 0,
+  f: 1,
+  bb: 2,
+  eb: 3,
+  ab: 4,
+  db: 5,
+  gb: 6,
+  cb: 7,
+  fb: 8
+};
+
+const notes = ['C','D','E','F','G','A','B'];
+
+module.exports = {  sharps,
+                    flats,
+                    notes  }
