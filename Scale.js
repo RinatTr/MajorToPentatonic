86bcd7ea6db4a21f97abe397b10e9756c9aa04a4
RinatTr <rinatreg@gmail.com>
2019-08-23 13:11:12 -0400

    import data

--- Scale.js
+++ Scale.js
@@ -1,28 +1,9 @@
+const { sharps, flats, notes } = require('./data.js');
+
 class Scale {
   constructor() {
-    this.sharps = {
-      'c':0,
-      'g':1,
-      'd':2,
-      'a':3,
-      'e':4,
-      'b':5,
-      'fs':6,
-      'cs':7,
-      'gs':8,
-      'ds':9,
-      'as':10
-    }
-    this.flats = {
-      'c':0,
-      'f':1,
-      'bb':2,
-      'eb':3,
-      'ab':4,
-      'db':5,
-      'gb':6,
-      'cb':7,
-      'fb':8
-    }
+    this.sharps = sharps;
+    this.flats = flats;
+    this.notes = notes;
   }
 }
